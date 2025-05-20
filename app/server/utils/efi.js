a45a921be6c3b8f8654f2009bfde9b0825a78b91
Caio Vieira <caioviier@gmail.com>
2025-05-20 18:28:17 -0300

    feat: including prisma and efi pay

---
+++ app/server/utils/efi.js
@@ -0,0 +1,13 @@
+
+import EfiPay from 'sdk-node-apis-efi'
+
+const options = {
+  clientId: process.env.EFI_CLIENT_ID,
+  clientSecret: process.env.EFI_CLIENT_SECRET,
+  environment: process.env.EFI_SANDBOX,
+  certificate: '../../../certs/Dev.p12'
+}
+
+const efipay = new EfiPay(options)
+
+export default efipay
