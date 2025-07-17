0ee03d5b6ef5c14cb4c19df1fda8ff273f02ff5d
Caio Vieira <caioviier@gmail.com>
2025-07-17 00:06:36 -0300

    chore: corrige artigos

--- app/components/about.vue
+++ app/components/about.vue
@@ -8,8 +8,8 @@
   />
   <div class="card">
     <p  class="mb-3 w-full">Para acessar o código desse projeto, basta <a class="text-primary font-bold" href="https://github.com/Caioohv/efi-pay-nuxt-example">clicar aqui</a>.</p>
-    <p class="mb-3">Este projeto é apenas uma demonstração de uso da API da Efí Pay, com emissão de boleto e pix, além da cobrança via cartão de crédito.</p>
-    <p class="mb-3">Foi usado o ambiente sandbox da Efí, logo, as informações exibidas não possuem valor.</p>
+    <p class="mb-3">Este projeto é apenas uma demonstração de uso da API do Efí Pay, com emissão de boleto e pix, além da cobrança via cartão de crédito.</p>
+    <p class="mb-3">Foi usado o ambiente sandbox do Efí, logo, as informações exibidas não possuem valor.</p>
     <p class="mb-3">Além disso, as informações (ver código) usadas para o pagamento, como o número do cartão, endereço e etc. são informações fictícias, sem valor real.</p>
     <p class="w-full">Caso tenha interesse em usar essa api, acesse <a class="text-primary font-bold" href="https://www.sejaefi.com.br">este link</a>.</p>
 
--- app/components/hero.vue
+++ app/components/hero.vue
@@ -3,7 +3,7 @@
     <h1 class="text-lg font-extrabold">Exemplo de utilização</h1>
     <p class="text-sm font-semibold mt-1">API de cobranças Efí Pay</p>
     <p class="text-xs font-light mt-3 mb-5 text-center">
-      Essas emissões são feitas no ambiente de homologação da Efí, portanto, não são válidas para pagamentos reais.
+      Essas emissões são feitas no ambiente de homologação do Efí, portanto, não são válidas para pagamentos reais.
     </p>
   </div>
 </template>
